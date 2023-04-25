ad058e2df59858c48fbc6c4c6e00654d44b3bf5c
Darío Rumbo <dario.rumbo@gmail.com>
2023-04-25 16:20:20 -0300

    v1.0 | Contador y detalles funcionando

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    let name = 'SvelteKit'
+    setTimeout(() => { name = 'Darío' }, 2000)
+</script>
+<h1>Welcome to {name}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
