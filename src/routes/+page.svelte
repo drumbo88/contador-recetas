30df9d5854eea5c46b7bea9468edec66d5c4f960
Darío Rumbo <dario.rumbo@gmail.com>
2023-04-25 17:21:29 -0300

    Separación de partes en componentes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
+    import App from "../components/App.svelte";
+
     let name = 'SvelteKit'
     setTimeout(() => { name = 'Darío' }, 2000)
 </script>
-<h1>Welcome to {name}</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<App />
